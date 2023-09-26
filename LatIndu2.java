b41928ec811ae1702e3c8449ecd365f5e3fc797c
afiqfirdaus21 <144087118+afiqfirdaus21@users.noreply.github.com>
2023-09-26 19:19:31 +0700

    Add files via upload

---
+++ LatIndu2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+ public class LatIndu2{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int username = 222;
+        int password = 111;
+        System.out.println("Masukkan Username dan Password");
+        System.out.println("Username   : ");
+        int user = input.nextInt();
+        System.out.println("Password   : ");
+        int pw = input.nextInt();
+        if (user == username && pw == password){
+            System.out.println("Selamat anda berhasil login!");
+        }else{
+            System.out.println("Harap cek kembali username dan password anda");
+        }
+    }
+ }
